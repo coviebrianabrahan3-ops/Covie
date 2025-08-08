f5e4b25cc7070536dd63a8c04b8d2d237d56a099
coviebrianabrahan3-ops <coviebrianabrahan3@gmail.com>
2025-08-08 14:48:15 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mystorage.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(["manage.py", "runserver", "127.0.0.1:8000"])
+  
